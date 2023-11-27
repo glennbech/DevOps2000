d94567d34fbb8b8b44f401e49d5514caa0f32ac6
AbdulHamidA <ahoukla@gmail.com>
2023-11-27 11:11:07 +0100

    updated src file

--- src/main/java/com/example/s3rekognition/services/RekognitionService.java
+++ src/main/java/com/example/s3rekognition/services/RekognitionService.java
@@ -22,7 +22,7 @@ import org.springframework.stereotype.Service;
 import java.util.ArrayList;
 import java.util.List;
 
-
+@Slf4j
 @Service
 public class RekognitionService implements ApplicationListener<ApplicationReadyEvent> {
 
